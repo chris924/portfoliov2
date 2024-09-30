ebaafe665a8947dec8f36c63c0d06ce9a6be49dc
chris924 <burkakrisztofer@hotmail.com>
2024-09-30 17:57:24 +0200

    feat: main title modifications

--- portfoliov2-frontend/src/pages/index.tsx
+++ portfoliov2-frontend/src/pages/index.tsx
@@ -7,18 +7,44 @@ import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
 import DefaultLayout from "@/layouts/default";
+import { motion } from "framer-motion";
 
 export default function IndexPage() {
   return (
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <div className="inline-block max-w-lg text-center justify-center">
-          <span className={title()}>Make&nbsp;</span>
-          <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
-          <br />
-          <span className={title()}>
-            websites regardless of your design experience.
-          </span>
+
+              {[...Array(3)].map((_, index) => (
+              <div key={index} className="tracking-tight m-3">
+        <motion.div 
+        initial={{ opacity: 0, y: 50, scale: 0.9 }} 
+        animate={{ opacity: 1, y: 0, scale: 1 }} 
+        transition={{ duration: 0.8, ease: [0.6, -0.05, 0.01, 0.99] }}
+      >
+        <motion.span 
+          initial={{ opacity: 0, x: -20 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ delay: 0.1, duration: 0.6, ease: "easeOut" }}
+          className={title({ class: "tracking-widest font-unisansheavy"})}
+        >
+          HEY,&nbsp;
+        </motion.span>
+        
+        <motion.span 
+          initial={{ opacity: 0, x: 20 }} 
+          animate={{ opacity: 1, x: 0 }} 
+          transition={{ delay: 0.2, duration: 0.6, ease: "easeOut" }}
+          className={title({ color: "green", class: "tracking-widest font-unisansheavy" })}
+        >
+          I'M CHRIS
+        </motion.span>
+        
+        <br />
+      </motion.div>
+          
+        </div>
+        ))}    
           <div className={subtitle({ class: "mt-4" })}>
             Beautiful, fast and modern React UI library.
           </div>
--- portfoliov2-frontend/tailwind.config.js
+++ portfoliov2-frontend/tailwind.config.js
@@ -10,7 +10,11 @@ module.exports = {
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+      unisansheavy: ['unisansheavy', 'sans-serif'],
+    },
+  },
   },
   darkMode: "class",
   plugins: [nextui()],
