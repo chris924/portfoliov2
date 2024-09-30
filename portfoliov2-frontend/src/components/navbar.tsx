4ef2bbd58d4edcd3bb66d15b945b041b44934aa8
chris924 <burkakrisztofer@hotmail.com>
2024-09-30 18:43:03 +0200

    feat: navbar animated

--- portfoliov2-frontend/src/components/navbar.tsx
+++ portfoliov2-frontend/src/components/navbar.tsx
@@ -2,6 +2,7 @@ import { Button } from "@nextui-org/button";
 import { Kbd } from "@nextui-org/kbd";
 import { Link } from "@nextui-org/link";
 import { Input } from "@nextui-org/input";
+import {animate, motion} from "framer-motion";
 import {
   Navbar as NextUINavbar,
   NavbarBrand,
@@ -49,98 +50,105 @@ export const Navbar = () => {
   );
 
   return (
+    <>
+    <motion.div initial= {{opacity:0, y: -50}} animate= {{opacity: 1, y: 0}} transition={{ duration: 0.8, ease: [0.6, -0.05, 0.01, 0.99] }}>
     <NextUINavbar maxWidth="xl" position="sticky">
-      <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
-        <NavbarBrand className="gap-3 max-w-fit">
-          <Link
-            className="flex justify-start items-center gap-1"
-            color="foreground"
-            href="/"
-          >
-            <Logo />
-            
-            <HoverInHackerTyper hoverFrom="up">Chris Portfolio Site</HoverInHackerTyper>
-          </Link>
-        </NavbarBrand>
-        <div className="hidden lg:flex gap-4 justify-start ml-2">
-          {siteConfig.navItems.map((item) => (
-            <NavbarItem key={item.href}>
-              <Link
-                className={clsx(
-                  linkStyles({ color: "foreground" }),
-                  "data-[active=true]:text-primary data-[active=true]:font-medium",
-                )}
-                color="foreground"
-                href={item.href}
-              >
-                <HoverInHackerTyper hoverFrom="up">{item.label}</HoverInHackerTyper>
-              </Link>
-            </NavbarItem>
-          ))}
-        </div>
-      </NavbarContent>
-
-      <NavbarContent
-        className="hidden sm:flex basis-1/5 sm:basis-full"
-        justify="end"
+  <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
+    <NavbarBrand className="gap-3 max-w-fit">
+      <Link
+        className="flex justify-start items-center gap-1"
+        color="foreground"
+        href="/"
       >
-        <NavbarItem className="hidden sm:flex gap-2">
-          <Link isExternal href={siteConfig.links.twitter} title="Twitter">
-            <TwitterIcon className="text-default-500" />
-          </Link>
-          <Link isExternal href={siteConfig.links.discord} title="Discord">
-            <DiscordIcon className="text-default-500" />
-          </Link>
-          <Link isExternal href={siteConfig.links.github} title="GitHub">
-            <GithubIcon className="text-default-500" />
-          </Link>
-          <ThemeSwitch />
-        </NavbarItem>
-        <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
-        <NavbarItem className="hidden md:flex">
-          <Button
-            isExternal
-            as={Link}
-            className="text-sm font-normal text-default-600 bg-default-100"
-            href={siteConfig.links.sponsor}
-            startContent={<HeartFilledIcon className="text-danger" />}
-            variant="flat"
+        <Logo />
+        
+        <HoverInHackerTyper hoverFrom="up">Chris Portfolio Site</HoverInHackerTyper>
+      </Link>
+    </NavbarBrand>
+    <div className="hidden lg:flex gap-4 justify-start ml-2">
+      {siteConfig.navItems.map((item) => (
+        <NavbarItem key={item.href}>
+          <Link
+            className={clsx(
+              linkStyles({ color: "foreground" }),
+              "data-[active=true]:text-primary data-[active=true]:font-medium",
+            )}
+            color="foreground"
+            href={item.href}
           >
-            Sponsor
-          </Button>
+            <HoverInHackerTyper hoverFrom="up">{item.label}</HoverInHackerTyper>
+          </Link>
         </NavbarItem>
-      </NavbarContent>
+      ))}
+    </div>
+  </NavbarContent>
 
-      <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-        <Link isExternal href={siteConfig.links.github}>
-          <GithubIcon className="text-default-500" />
-        </Link>
-        <ThemeSwitch />
-        <NavbarMenuToggle />
-      </NavbarContent>
+  <NavbarContent
+    className="hidden sm:flex basis-1/5 sm:basis-full"
+    justify="end"
+  >
+    <NavbarItem className="hidden sm:flex gap-2">
+      <Link isExternal href={siteConfig.links.twitter} title="Twitter">
+        <TwitterIcon className="text-default-500" />
+      </Link>
+      <Link isExternal href={siteConfig.links.discord} title="Discord">
+        <DiscordIcon className="text-default-500" />
+      </Link>
+      <Link isExternal href={siteConfig.links.github} title="GitHub">
+        <GithubIcon className="text-default-500" />
+      </Link>
+      <ThemeSwitch />
+    </NavbarItem>
+    <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
+    <NavbarItem className="hidden md:flex">
+      <Button
+        isExternal
+        as={Link}
+        className="text-sm font-normal text-default-600 bg-default-100"
+        href={siteConfig.links.sponsor}
+        startContent={<HeartFilledIcon className="text-danger" />}
+        variant="flat"
+      >
+        Sponsor
+      </Button>
+    </NavbarItem>
+  </NavbarContent>
 
-      <NavbarMenu>
-        {searchInput}
-        <div className="mx-4 mt-2 flex flex-col gap-2">
-          {siteConfig.navMenuItems.map((item, index) => (
-            <NavbarMenuItem key={`${item}-${index}`}>
-              <Link
-                color={
-                  index === 2
-                    ? "primary"
-                    : index === siteConfig.navMenuItems.length - 1
-                      ? "danger"
-                      : "foreground"
-                }
-                href="#"
-                size="lg"
-              >
-               <HoverInHackerTyper hoverFrom="up">{item.label}</HoverInHackerTyper>
-              </Link>
-            </NavbarMenuItem>
-          ))}
-        </div>
-      </NavbarMenu>
-    </NextUINavbar>
-  );
+  <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
+    <Link isExternal href={siteConfig.links.github}>
+      <GithubIcon className="text-default-500" />
+    </Link>
+    <ThemeSwitch />
+    <NavbarMenuToggle />
+  </NavbarContent>
+
+  <NavbarMenu>
+    {searchInput}
+    <div className="mx-4 mt-2 flex flex-col gap-2">
+      {siteConfig.navMenuItems.map((item, index) => (
+        <NavbarMenuItem key={`${item}-${index}`}>
+          <Link
+            color={
+              index === 2
+                ? "primary"
+                : index === siteConfig.navMenuItems.length - 1
+                  ? "danger"
+                  : "foreground"
+            }
+            href="#"
+            size="lg"
+          >
+           <HoverInHackerTyper hoverFrom="up">{item.label}</HoverInHackerTyper>
+          </Link>
+        </NavbarMenuItem>
+      ))}
+    </div>
+  </NavbarMenu>
+</NextUINavbar>
+
+</motion.div>
+
+</>
+);
+   
 };
