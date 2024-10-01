92ba2c69310ae4cf19e23a74d41f92e752eea3db
chris924 <burkakrisztofer@hotmail.com>
2024-10-01 17:45:51 +0200

    feat: mainText refactor

---
+++ portfoliov2-frontend/src/components/mainText.tsx
@@ -0,0 +1,55 @@
+import DefaultLayout from "@/layouts/default"
+import { motion } from "framer-motion"
+import { title, subtitle } from "./primitives"
+
+
+export default function MainText() {
+    return (
+              <>
+               <DefaultLayout>
+          <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+            <div className="inline-block max-w-lg text-center justify-center">
+    
+                  {[...Array(3)].map((_, index) => (
+                  <div key={index} className="tracking-tight m-3">
+            <motion.div 
+            initial={{ opacity: 0, y: 50, scale: 0.9 }} 
+            animate={{ opacity: 1, y: 0, scale: 1 }} 
+            transition={{ duration: 0.8, ease: [0.6, -0.05, 0.01, 0.99] }}
+          >
+            <motion.span 
+              initial={{ opacity: 0, x: -20 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ delay: 0.1, duration: 0.6, ease: "easeOut" }}
+              className={title({ class: "tracking-widest font-unisansheavy"})}
+            >
+              HEY,&nbsp;
+            </motion.span>
+            
+            <motion.span 
+              initial={{ opacity: 0, x: 20 }} 
+              animate={{ opacity: 1, x: 0 }} 
+              transition={{ delay: 0.2, duration: 0.6, ease: "easeOut" }}
+              className={title({ color: "green", class: "tracking-widest font-unisansheavy text-opacity-0" })}
+            >
+              I'M CHRIS
+            </motion.span>
+            
+            <br />
+          </motion.div>
+              
+            </div>
+            ))}    
+              <div className={subtitle({ class: "mt-4" })}>
+                Beautiful, fast and modern React UI library.
+              </div>
+            </div>              
+    
+          </section>
+        </DefaultLayout>
+              </>
+
+    )
+
+}
+             
--- portfoliov2-frontend/src/pages/index.tsx
+++ portfoliov2-frontend/src/pages/index.tsx
@@ -12,6 +12,7 @@ import RevealOnScroll from "@/components/animations/revealonscroll";
 import { useState } from "react";
 import LevelViewer from "@/components/animations/leverviewer";
 import "../styles/globals.css"
+import MainText from "@/components/mainText";
 
 
 
@@ -45,83 +46,9 @@ export default function IndexPage() {
       </div>
 
       {!overlayVisible && (
-          <>
-           <DefaultLayout>
-      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-        <div className="inline-block max-w-lg text-center justify-center">
-
-              {[...Array(3)].map((_, index) => (
-              <div key={index} className="tracking-tight m-3">
-        <motion.div 
-        initial={{ opacity: 0, y: 50, scale: 0.9 }} 
-        animate={{ opacity: 1, y: 0, scale: 1 }} 
-        transition={{ duration: 0.8, ease: [0.6, -0.05, 0.01, 0.99] }}
-      >
-        <motion.span 
-          initial={{ opacity: 0, x: -20 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ delay: 0.1, duration: 0.6, ease: "easeOut" }}
-          className={title({ class: "tracking-widest font-unisansheavy"})}
-        >
-          HEY,&nbsp;
-        </motion.span>
-        
-        <motion.span 
-          initial={{ opacity: 0, x: 20 }} 
-          animate={{ opacity: 1, x: 0 }} 
-          transition={{ delay: 0.2, duration: 0.6, ease: "easeOut" }}
-          className={title({ color: "green", class: "tracking-widest font-unisansheavy" })}
-        >
-          I'M CHRIS
-        </motion.span>
-        
-        <br />
-      </motion.div>
           
-        </div>
-        ))}    
-          <div className={subtitle({ class: "mt-4" })}>
-            Beautiful, fast and modern React UI library.
-          </div>
-        </div>
-
-        <div className="flex gap-3">
-          <Link
-            isExternal
-            className={buttonStyles({
-              color: "primary",
-              radius: "full",
-              variant: "shadow",
-            })}
-            href={siteConfig.links.docs}
-          >
-            Documentation
-          </Link>
-          <Link
-            isExternal
-            className={buttonStyles({ variant: "bordered", radius: "full" })}
-            href={siteConfig.links.github}
-          >
-            <GithubIcon size={20} />
-            GitHub
-          </Link>
-        </div>
-
-        <div className="mt-8">
-          <Snippet hideCopyButton hideSymbol variant="bordered">
-            <span>
-              Get started by editing{" "}
-              <RevealOnScroll>TEST</RevealOnScroll>
-              <Code color="primary">pages/index.tsx</Code>
-            </span>
-          </Snippet>
-        </div>
-
+          <MainText/>
            
-
-      </section>
-    </DefaultLayout>
-          </>
         )}
    
 </>
