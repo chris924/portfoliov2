ab918e32f6971bbcd53861dbb4972aa0003a0ed4
chris924 <burkakrisztofer@hotmail.com>
2024-10-09 19:19:10 +0200

    feat: contactform, email sending functionality

---
+++ portfoliov2-frontend/src/components/contactForm.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
+
+function ContactForm() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleChange = (e: any) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const sendEmail = (e: any) => {
+    e.preventDefault();
+    
+    
+    emailjs.send(
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        formData,
+        import.meta.env.VITE_EMAILJS_USER_ID
+      )
+      .then(
+        (result) => {
+          console.log('Email sent successfully:', result.text);
+          alert('Message sent!');
+        },
+        (error) => {
+          console.log('Failed to send email:', error.text);
+        }
+      );
+  };
+
+  return (
+    <form className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full" onSubmit={sendEmail}>
+      <input
+        type="text"
+        name="name"
+        placeholder="Your name"
+        onChange={handleChange}
+        value={formData.name}
+        className="border-b border-gray-400 focus:outline-none focus:border-black p-2"
+      />
+      <input
+        type="email"
+        name="email"
+        placeholder="Your email"
+        onChange={handleChange}
+        value={formData.email}
+        className="border-b border-gray-400 focus:outline-none focus:border-black p-2"
+      />
+      <textarea
+        name="message"
+        placeholder="Your message"
+        onChange={handleChange}
+        value={formData.message}
+        className="border-b border-gray-400 focus:outline-none focus:border-black p-2 md:col-span-2"
+      />
+      <button
+        type="submit"
+        className="mt-4 bg-black text-white py-2 px-4 rounded-md md:col-span-2"
+      >
+        Send Message
+      </button>
+    </form>
+  );
+}
+
+export default ContactForm;
--- portfoliov2-frontend/src/components/contactText.tsx
+++ portfoliov2-frontend/src/components/contactText.tsx
@@ -1,31 +1,16 @@
+import RevealOnScroll from "./animations/revealonscroll";
+import ContactForm from "./contactForm";
+
 export default function ContactText() {
     return (
+      <RevealOnScroll>
       <section className="flex flex-col md:flex-row w-full max-w-8xl mx-auto gap-16 py-12 mt-40">
         {/* Left Side */}
         <div className="flex-[2.6] flex flex-col justify-start space-y-6 mr-10">
           <h1 className="text-8xl font-bold mb-5">Contact me here</h1>
 
-          <form className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
-            <input
-              type="text"
-              placeholder="Your name"
-              className="border-b border-gray-400 focus:outline-none focus:border-black p-2"
-            />
-            <input
-              type="email"
-              placeholder="Your email"
-              className="border-b border-gray-400 focus:outline-none focus:border-black p-2"
-            />
-            <textarea
-              placeholder="Your message"
-              className="border-b border-gray-400 focus:outline-none focus:border-black p-2 md:col-span-2"
-            />
-            <button className="mt-4 bg-black text-white py-2 px-4 rounded-md md:col-span-2">
-              Send Message
-            </button>
-          </form>
+        <ContactForm/>
         </div>
-  
         {/* Right Side */}
         <div className="flex-1 flex flex-col space-y-4 ml-auto">
      
@@ -50,5 +35,6 @@ export default function ContactText() {
           </div>
         </div>
       </section>
+      </RevealOnScroll>
     );
   }
