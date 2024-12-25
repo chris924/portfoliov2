8e2c24e7620372990ea206c398834fd9848aa6a4
chris924 <burkakrisztofer@hotmail.com>
2024-12-25 18:05:29 +0100

    fix: removed unused console log

--- portfoliov2-frontend/src/components/contactForm.tsx
+++ portfoliov2-frontend/src/components/contactForm.tsx
@@ -19,10 +19,6 @@ function ContactForm() {
   const sendEmail = (e: any) => {
     e.preventDefault();
     
-    console.log("TEST:",  import.meta.env.VITE_EMAILJS_SERVICE_ID,
-      import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
-      formData,
-      import.meta.env.VITE_EMAILJS_USER_ID)
 
 
     emailjs.send(
