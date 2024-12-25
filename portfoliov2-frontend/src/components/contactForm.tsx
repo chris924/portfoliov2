ef42a3b501ab168f07b05e07b0203696b34da749
chris924 <burkakrisztofer@hotmail.com>
2024-12-25 17:55:34 +0100

    chore. emailjs test

--- portfoliov2-frontend/src/components/contactForm.tsx
+++ portfoliov2-frontend/src/components/contactForm.tsx
@@ -19,7 +19,12 @@ function ContactForm() {
   const sendEmail = (e: any) => {
     e.preventDefault();
     
-    
+    console.log("TEST:",  import.meta.env.VITE_EMAILJS_SERVICE_ID,
+      import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+      formData,
+      import.meta.env.VITE_EMAILJS_USER_ID)
+
+
     emailjs.send(
         import.meta.env.VITE_EMAILJS_SERVICE_ID,
         import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
